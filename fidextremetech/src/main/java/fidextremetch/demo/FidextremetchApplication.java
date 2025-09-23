7897b84bdb423fae9863b108f9e03362d6a11e96
ERICK BARRIENTOS MONTES <83951741+erickbm2002@users.noreply.github.com>
2025-09-22 22:02:21 -0600

    se termina la pagina con sus 3 secciones

---
+++ fidextremetech/src/main/java/fidextremetch/demo/FidextremetchApplication.java
@@ -0,0 +1,13 @@
+package fidextremetch.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FidextremetchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FidextremetchApplication.class, args);
+	}
+
+}
