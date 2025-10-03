5c5ba9f077f7ee2185fb70e41a4c021d04d16ec5
ERICK BARRIENTOS MONTES <83951741+erickbm2002@users.noreply.github.com>
2025-10-02 18:14:54 -0600

    se trabaja con la base de datos y la conexiones domain,services,repository

--- fidextremetech/src/main/java/fidextremetch/FidextremetchApplication.java
+++ fidextremetech/src/main/java/fidextremetch/FidextremetchApplication.java
@@ -1,4 +1,4 @@
-package fidextremetch.demo;
+package fidextremetch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ fidextremetech/src/main/java/fidextremetch/repository/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package fidextremetch.repository;
+ 
+import fidextremetch   .domain.Categoria;
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+ 
+public interface CategoriaRepository extends JpaRepository<Categoria,Integer>{
+    public List<Categoria> findByActivoTrue();
+ 
+}
